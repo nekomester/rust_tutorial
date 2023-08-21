47a24ba7d6836049d35a678e865189f8477ff65e
tamkem <5010744+nekomester@users.noreply.github.com>
2023-08-21 16:28:35 +0900

    issue-011: 5.構造体

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -31,6 +31,22 @@ struct Rectangle {
     height: u32,
 }
 
+// struct文脈でメソッドを定義する際はimpl(implementation; 実装)
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    // 引数にselfを取らない例. コンストラクタ等.
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
 
@@ -38,10 +54,20 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    )
+        rect1.area()
+    );
+
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let rect4 = Rectangle::square(7);
+    println!("square is {}.", rect4.area());
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
+// メソッド定義で不要となる
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
